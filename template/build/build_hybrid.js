7d78467242e30cc492a69a990c80ec7fcb2d35d4
F-loat <945852046@qq.com>
2017-02-04 18:29:32 +0800

    update build_hybrid.js

--- template/build/build_hybrid.js
+++ template/build/build_hybrid.js
@@ -1,13 +1,14 @@
 // https://github.com/shelljs/shelljs
 require('./check-versions')()
-require('shelljs/global')
-env.NODE_ENV = 'production'
 
-var path = require('path')
-var config = require('../config')
-config.build.assetsPublicPath = ''
+process.env.NODE_ENV = 'production'
+
 var ora = require('ora')
+var path = require('path')
+var chalk = require('chalk')
+var shell = require('shelljs')
 var webpack = require('webpack')
+var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 
 function HtmlAddScript (options) {
@@ -26,23 +27,20 @@ HtmlAddScript.prototype.apply = function(compiler) {
   })
 }
 
+config.build.assetsPublicPath = ''
 webpackConfig.plugins.push(new HtmlAddScript({
   paths: ['cordova.js']
 }))
-
-console.log(
-  '  Tip:\n' +
-  '  Built files are meant to be served over an HTTP server.\n' +
-  '  Opening index.html over file:// won\'t work.\n'
-)
 
 var spinner = ora('building for production...')
 spinner.start()
 
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
-rm('-rf', assetsPath)
-mkdir('-p', assetsPath)
-cp('-R', 'static/*', assetsPath)
+shell.rm('-rf', assetsPath)
+shell.mkdir('-p', assetsPath)
+shell.config.silent = true
+shell.cp('-R', 'static/*', assetsPath)
+shell.config.silent = false
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
@@ -53,5 +51,11 @@ webpack(webpackConfig, function (err, stats) {
     children: false,
     chunks: false,
     chunkModules: false
-  }) + '\n')
+  }) + '\n\n')
+
+  console.log(chalk.cyan('  Build complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ))
 })
